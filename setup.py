6f5418acf07c41503634dbd4da55dd6d3b82f47e
Phil Reinhold <pcreinhold@gmail.com>
2014-11-07 17:53:21 -0500

    Fix py2exe setup

--- setup.py
+++ setup.py
@@ -11,7 +11,9 @@ setup(
         ('', ['icon.ico'])],
     options={
         "py2exe":
-            { "includes":["h5py.defs", "h5py.utils", "h5py._proxy", "h5py.h5ac", "scipy.sparse.csgraph._validation"],
+            { "includes":
+                  ["h5py.defs", "h5py.utils", "h5py._proxy", "h5py.h5ac",
+                   "scipy.sparse.csgraph._validation", "scipy.special._ufuncs_cxx"],
               "dll_excludes":["MSVCP90.dll", "libzmq.pyd"]  }
     }
 )
